fb7112d1f0c1d8e81e3ee932f0ecb1d7ca78d0a5
SpudGunMan <kellykeeton@yahoo.com>
2024-03-13 23:52:47 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@
 WINE_FONT_DIR="$HOME/.wine/drive_c/windows/Fonts/"
 
 if [ -d $WINE_FONT_DIR ]; then
+    echo "Installing fonts..."
     cp font/segoeui.ttf "$WINE_FONT_DIR"/segoeui.ttf > /dev/null 2>&1 # regular
     cp font/seguiemj.ttf "$WINE_FONT_DIR"/seguiemj.ttf > /dev/null 2>&1 # regular
     cp font/ahronbd.ttf "$WINE_FONT_DIR"/ahronbd.ttf > /dev/null 2>&1 # bold
@@ -15,4 +16,7 @@ if [ -d $WINE_FONT_DIR ]; then
     cp font/msyhbd.ttf "$WINE_FONT_DIR"/msyhbd.ttf > /dev/null 2>&1 # bold
     cp font/msyh.ttf "$WINE_FONT_DIR"/msyh.ttf > /dev/null 2>&1 # regular
 fi
+
+echo "All fonts installed"
+exit 0
 
